d4bfb67fcd57514d7b2349d5d35d21aec7df9b3b
garana <wakamaro1006@gmail.com>
2025-08-15 10:26:08 +0900

    OrganizeMainSceneで全ファイル削除後、ロード時もTitleSceneの”思い出すボタン”を非表示にする機能を追加

--- Assets/Script/TitleScene/HideButtonAfterDeletingAllFiles.cs
+++ Assets/Script/TitleScene/HideButtonAfterDeletingAllFiles.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 /// </summary>
 public class HideButtonAfterDeletingAllFiles : MonoBehaviour
 {
+    private bool allFilesDeleted = false; // game_save.jsonのallFilesCompletelyDeletedフラグをチェック
 
     /// <summary>
     /// Startメソッド - シーン開始時の処理
     /// </summary>
     private void Start()
     {
-        // OrganizeMainSceneで全ファイル削除後、確認ダイアログで”はい”押下時
-        if (OrganizeMainSceneController.returnScene == true)
+        // GameSaveManagerから全ファイル完全削除フラグを取得
+        allFilesDeleted = GameSaveManager.Instance.GetAllFilesCompletelyDeleted();
+        //Debug.Log("HideButtonAfterDeletingAllFiles : allFilesDeletedフラグ" + allFilesDeleted);
+
+        // OrganizeMainSceneで全ファイル削除後、確認ダイアログで"はい"押下時
+        // または、game_save.jsonのallFilesCompletelyDeletedフラグがtrueの場合
+        if (OrganizeMainSceneController.returnScene == true || allFilesDeleted == true)
         {
-            // Debug.Log("HideButtonAfterDeletingAllFiles : Buttonを非表示にする");
+            //Debug.Log("HideButtonAfterDeletingAllFiles : Buttonを非表示にする");
 
             // このスクリプトがアタッチされているButtonを非表示にする
             gameObject.SetActive(false);
